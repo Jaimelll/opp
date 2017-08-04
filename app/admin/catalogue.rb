8f09fc32375f8554f71da58cbca18b0a9750ba2f
Jaimelll <jaduran0102@gmail.com>
2017-08-04 09:28:39 -0500

    encargo

--- app/admin/catalogue.rb
+++ app/admin/catalogue.rb
@@ -11,8 +11,63 @@ ActiveAdmin.register Catalogue do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+menu  priority: 5, label: "Clase"
 permit_params :clase, :descripcion, :orden,
      :admin_user_id
+
+filter :clase
+filter :descripcion
+filter :orden
+
+
+
+
+     index :title => 'Lista de Clases' do
+
+             column("clase")
+             column("descripcion")
+             column("orden")
+
+
+
+              actions
+          end
+          form :title => 'Edicion Clase'  do |f|
+
+
+                  f.inputs  do
+
+                    f.input :clase, :input_html => { :style =>  'width:30%'}
+                    f.input :descripcion
+                    f.input :orden, :input_html => { :style =>  'width:30%'}
+                    f.input :admin_user_id, :input_html => { :value => current_admin_user.id }, :as => :hidden
+
+                           f.actions
+
+                    end
+
+          end
+
+          show :title => ' Clase'  do
+
+                attributes_table  do
+
+
+
+
+
+                row :clase
+                row :descripcion
+                row :orden
+
+
+
+                end
+              end
+
+
+
+
 
 
 end
