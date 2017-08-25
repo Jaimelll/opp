13c5e36c9efb015fe47547ca10187012bf901092
Jaimelll <jaduran0102@gmail.com>
2017-08-25 11:12:05 -0500

    encargo

--- app/admin/supplier.rb
+++ app/admin/supplier.rb
@@ -12,4 +12,21 @@ ActiveAdmin.register Supplier do
 #   permitted
 # end
 
+menu  priority: 8, label: "Proveedores"
+
+permit_params :numero_proveedor, :num_documento_pais, :des_proveedor,
+ :direccion_pais, :telefono_pais, :correo_pais, :pag_weeb, :nro_representante,
+ :des_representante, :correo_representante, :activo, :fec_registro, :mod_registro,
+  :tipo_proveedor, :calificacion, :observacion, :admin_user_id
+
+
+  
+
+
+
+
+
+
+
+
 end
